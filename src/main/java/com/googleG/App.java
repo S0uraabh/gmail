b7884ffaaaf523218a422dbf379853552d372418
Saurabh <saurabhbarmase038gmail.com>
2025-01-01 15:38:50 +0530

    edited HNY

--- src/main/java/com/googleG/App.java
+++ src/main/java/com/googleG/App.java
@@ -5,6 +5,6 @@ package com.googleG;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Happy New Year");
     }
 }
